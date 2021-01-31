bfe95500e7c529b53abdfce3f7d2a0fb4ae003f5
CL Mason <clsoar@gmail.com>
2021-01-31 15:26:44 -0500

    firebase added

---
+++ public/js/firebase-init.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyDbC_xe3Gewx_Z8eAhIboiT1EgQuJNzClE",
+  authDomain: "county-line-communications.firebaseapp.com",
+  projectId: "county-line-communications",
+  storageBucket: "county-line-communications.appspot.com",
+  messagingSenderId: "759805278863",
+  appId: "1:759805278863:web:dd5225fb791b15f16b8efa",
+  measurementId: "G-LTNLD6N370"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
